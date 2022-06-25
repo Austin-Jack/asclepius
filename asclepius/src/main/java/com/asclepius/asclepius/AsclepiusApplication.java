d6d6d56050811367c18c5ca0b513d7f9e7267827
shengningyi <287369241@qq.com>
2022-06-25 16:23:18 +0800

    feat:init project

---
+++ asclepius/src/main/java/com/asclepius/asclepius/AsclepiusApplication.java
@@ -0,0 +1,13 @@
+package com.asclepius.asclepius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsclepiusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsclepiusApplication.class, args);
+    }
+
+}
---
+++ asclepius/src/test/java/com/asclepius/asclepius/AsclepiusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asclepius.asclepius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsclepiusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
